2df900b98e79172ca99c4ce388b1da21999a78a0
Jayanta Mondal <jayanta551905@gmail.com>
2023-09-29 23:13:34 +0600

    title update

--- src/app/projects/layout.js
+++ src/app/projects/layout.js
@@ -1,3 +1,10 @@
+export async function generateMetadata() {
+  // SEO DATA FETCH
+  return {
+    title: "Projects",
+  };
+}
+
 export default function Layout({ children }) {
   return <div>{children}</div>;
 }
--- src/app/services/layout.js
+++ src/app/services/layout.js
@@ -1,3 +1,10 @@
+export async function generateMetadata() {
+  // SEO DATA FETCH
+  return {
+    title: "Services",
+  };
+}
+
 export default function Layout({ children }) {
   return <div>{children}</div>;
 }
